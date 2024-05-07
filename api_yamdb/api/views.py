0ccbc4ad104e68624e6d69cb4a22376cba77b3a7
Людмила Зозуля <zozulyalud@yandex.ru>
2024-05-07 21:15:16 +0300

    накидала вьюсетов по имеющимся моделям

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Genre, Title, GenreTitle, Review, Comment
+from .serializers import (
+    CategorySerializer,
+    GenreSerializer,
+    TitleSerializer,
+    GenreTitleSerializer,
+    ReviewSerializer,
+    CommentSerializer,
+)
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    """Представление для работы с моделью категория."""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    search_fields = ('name', )
+    lookup_field = 'slug'
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    """Представление для работы с моделью жанр."""
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    search_fields = ('name', )
+    lookup_field = 'slug'
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """Представление для работы с моделью титл."""
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+
+class GenreTitleViewSet(viewsets.ModelViewSet):
+    """Представление для работы с моделью микс титл и жанр."""
+    queryset = GenreTitle.objects.all()
+    serializer_class = GenreTitleSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """Представление для работы с моделью отзыв."""
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """Представление для работы с моделью коммент."""
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_review(self):
+        """Получаем отзыв для комментария."""
+        return get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'),
+            title__id=self.kwargs.get('title_id')
+        )
+
+    def get_queryset(self):
+        """Получаем queryset."""
+        return self.get_review().comments_review.all()
+
+    def perform_create(self, serializer):
+        """Переопределяем метод create."""
+        serializer.save(review=self.get_review(), author=self.request.user)
