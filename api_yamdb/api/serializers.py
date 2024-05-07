17f45457693a6f1f96b4a40d9ceaa359e6b0c31b
Людмила Зозуля <zozulyalud@yandex.ru>
2024-05-07 21:53:29 +0300

    правки от форматтеров и пайлинта

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,44 +1,52 @@
 from rest_framework import serializers
-from reviews.models import (
-    Category, Comment,
-    Genre, GenreTitle, Review, Title
-)
+from reviews.models import Category, Comment, Genre, GenreTitle, Review, Title
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Категории."""
     class Meta:
         model = Category
-        fields = ('id', 'name', 'slug')
+        fields = ("id", "name", "slug")
 
 
 class GenreSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Жанра."""
     class Meta:
         model = Genre
-        fields = ('id', 'name', 'slug')
+        fields = ("id", "name", "slug")
 
 
 class TitleSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Произведения."""
     class Meta:
         model = Title
-        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+        fields = ("id", "name", "year", "description", "genre", "category")
 
 
 class GenreTitleSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Соответствия жанра и произведения."""
     class Meta:
         model = GenreTitle
-        fields = ('id', 'genre', 'title')
+        fields = ("id", "genre", "title")
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    pub_date = serializers.DateTimeField(source='publishedmodel.pub_date', read_only=True)
+    """Сериализатор для модели Отзыва."""
+    pub_date = serializers.DateTimeField(
+        source="publishedmodel.pub_date", read_only=True
+    )
+
     class Meta:
         model = Review
-        fields = ('id', 'text', 'author', 'score', 'title', 'pub_date')
+        fields = ("id", "text", "author", "score", "title", "pub_date")
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    pub_date = serializers.DateTimeField(source='publishedmodel.pub_date', read_only=True)
+    """Сериализатор для модели Комментария."""
+    pub_date = serializers.DateTimeField(
+        source="publishedmodel.pub_date", read_only=True
+    )
+
     class Meta:
         model = Comment
-        fields = ('id', 'text', 'author', 'review', 'pub_date')
-        
+        fields = ("id", "text", "author", "review", "pub_date")
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from .models import Category, Genre, Title, GenreTitle, Review, Comment
-from .serializers import (
+from reviews.models import Category, Genre, Title, GenreTitle, Review, Comment
+from api.serializers import (
     CategorySerializer,
     GenreSerializer,
     TitleSerializer,
@@ -9,38 +10,49 @@ from .serializers import (
     CommentSerializer,
 )
 
+
 class CategoryViewSet(viewsets.ModelViewSet):
     """Представление для работы с моделью категория."""
+
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    search_fields = ('name', )
-    lookup_field = 'slug'
+    search_fields = ("name",)
+    lookup_field = "slug"
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     """Представление для работы с моделью жанр."""
+
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    search_fields = ('name', )
-    lookup_field = 'slug'
+    search_fields = ("name",)
+    lookup_field = "slug"
+
 
 class TitleViewSet(viewsets.ModelViewSet):
     """Представление для работы с моделью титл."""
+
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
 
+
 class GenreTitleViewSet(viewsets.ModelViewSet):
     """Представление для работы с моделью микс титл и жанр."""
+
     queryset = GenreTitle.objects.all()
     serializer_class = GenreTitleSerializer
 
+
 class ReviewViewSet(viewsets.ModelViewSet):
     """Представление для работы с моделью отзыв."""
+
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
+
 class CommentViewSet(viewsets.ModelViewSet):
     """Представление для работы с моделью коммент."""
+
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
@@ -48,8 +60,8 @@ class CommentViewSet(viewsets.ModelViewSet):
         """Получаем отзыв для комментария."""
         return get_object_or_404(
             Review,
-            id=self.kwargs.get('review_id'),
-            title__id=self.kwargs.get('title_id')
+            id=self.kwargs.get("review_id"),
+            title__id=self.kwargs.get("title_id"),
         )
 
     def get_queryset(self):
