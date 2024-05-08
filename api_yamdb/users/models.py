04af8b769ad9f64ef06fc9fd730127043a696e97
Козьмин Артём <kozmin_arti@mail.ru>
2024-05-08 15:35:54 +0700

    models.py сделан CustomUser и Role

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,12 +1,30 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-roles = [
-    'user',
-    'moderator',
-    'admin',
-    'super_admin'
-]
+class Role(models.Model):
+    """Класс ролей."""
+    ROLE_CHOICES = [
+        ('admin', 'Администратор'),
+        ('user', 'Пользователь'),
+        ('moderator', 'Модератор'),
+        ('super_admin', 'Суперпользователь')
+    ]
+
+    name = models.CharField(max_length=20,
+                            choices=ROLE_CHOICES,
+                            unique=True,
+                            )
+
+    def __str__(self):
+        return self.name
+
 
 class CustomUser(AbstractUser):
-    role = models.SlugField("Роль", blank=True)
+    """Переопределяем модель стандартного юзера."""
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
+
+    def save(self, *args, **kwargs):
+        """Переопределяем метод save для автоматического присваивания user."""
+        if not self.pk:
+            self.role = Role.objects.get(name='user')
+        super().save(*args, **kwargs)
