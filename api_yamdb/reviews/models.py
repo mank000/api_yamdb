49cf49b76b70148e20ae20497075037e81d40852
Andrey Zyryanov <Andrey.m.zyrianov@yandex.ruc:/Data/ADTest/stepik/venv/Scripts/python.exe>
2024-05-18 12:08:12 +0300

    reviews.py (meta наследование 4 класса), admin.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -60,7 +60,7 @@ class CommentReviewModel(models.Model):
 class Category(CategoryGenreModel):
     """Класс категорий."""
 
-    class Meta:
+    class Meta(CategoryGenreModel.Meta):
         verbose_name = "категория"
         verbose_name_plural = "Категории"
 
@@ -68,7 +68,7 @@ class Category(CategoryGenreModel):
 class Genre(CategoryGenreModel):
     """Класс жанров."""
 
-    class Meta:
+    class Meta(CategoryGenreModel.Meta):
         verbose_name = "жанр"
         verbose_name_plural = "Жанры"
 
@@ -152,7 +152,7 @@ class Review(CommentReviewModel):
         null=True,
     )
 
-    class Meta:
+    class Meta(CommentReviewModel.Meta):
         verbose_name = "отзыв"
         verbose_name_plural = "Отзывы"
         constraints = (
@@ -173,6 +173,6 @@ class Comment(CommentReviewModel):
         verbose_name="oтзыв",
     )
 
-    class Meta:
+    class Meta(CommentReviewModel.Meta):
         verbose_name = "комментарий"
         verbose_name_plural = "Комментарии"
